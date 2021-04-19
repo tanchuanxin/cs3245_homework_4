156a2557c005adc0568d56d366b67d0789f89167
Chen Ning <chenningg97@gmail.com>
2021-04-19 12:24:22 +0800

    Added clean.py

---
+++ clean.py
@@ -0,0 +1,78 @@
+import re
+import string
+import requests
+import nltk
+
+# Initialize porter stemmer
+ps = nltk.stem.PorterStemmer()
+
+
+class Clean:
+    def clean(self, text, zone):
+        if zone == "date_posted":
+
+        else:
+            text = self.remove_js(text)
+            text = self.remove_illegal_chars(text)
+            text = self.make_lowercase(text)
+            text = self.britishize(text)
+            text = self.tokenize(text)
+            text = self.stem(text)
+
+        return text
+
+    def remove_js(self, text):
+        # Find first occurance of javascript code
+        js_start = text.find('//<![CDATA')
+
+        # While we can find the start of javascript code, find the end
+        while js_start != -1:
+            js_end = text.find('//]]>', js_start)
+
+            # If no end, we won't know where to stop deleting, so just ignore it
+            if js_end == -1:
+                break
+
+            # Remove the code through string splicing
+            text = text[:js_start] + text[js_end + len('//]]>'):]
+
+            # Find next occurance of javascript
+            js_start = text.find('//<![CDATA')
+
+        return text
+
+    # Changes American english to British english
+    def britishize(self, text):
+        url = "https://raw.githubusercontent.com/hyperreality/American-British-English-Translator/master/data/american_spellings.json"
+        american_to_british_dict = requests.get(url).json()
+
+        for american_spelling, british_spelling in american_to_british_dict.items():
+            text = text.replace(american_spelling, british_spelling)
+
+        return text
+
+    def remove_illegal_chars(self, text):
+        # Remove punctuations
+        text = text.translate(str.maketrans('', '', string.punctuation))
+
+        # Remove all non alphanumeric characters and punctuation
+
+        text = re.sub("[^0-9a-zA-Z]+", " ", text)
+
+        return text
+
+    def make_lowercase(self, text):
+        # Convert text to lower case
+        text = text.lower()
+
+        return text
+
+    def tokenize(self, text):
+        text = nltk.word_tokenize(text)  # Tokenize by word
+
+        return text
+
+    def stem(self, text):
+        text = [ps.stem(word) for word in text]  # Stem every word
+
+        return text
--- index.py
+++ index.py
@@ -11,8 +11,14 @@ import string
 from progress.bar import Bar
 from progress.spinner import Spinner
 
+# Import own files
+from clean import Clean
+
 # Global definitions
 csv.field_size_limit(2**30)
+
+# Create instances of imported classes
+cleaner = Clean()
 
 
 def usage():
@@ -23,21 +29,6 @@ def usage():
     )
 
 
-def read_dataset(in_file):
-    # Open dataset file
-    with open(in_file, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-
-        # Create a dictionary to store the data and store all doc_ids
-        data = {}
-        doc_ids = []
-
-        # Load all data from dataset into the dict
-        load_documents_bar = Bar("Loading in documents", max=17153)
-
-        return data
-
-
 def build_index(in_file, out_dict, out_postings):
     """
     Build index from documents stored in the input directory,
@@ -46,23 +37,51 @@ def build_index(in_file, out_dict, out_postings):
     print("Indexing...")
 
     # Read in documents to index
-    with open(in_file, newline='', encoding='utf-8') as csvfile:
-        # Create a dictionary to store the data and store all doc_ids
-        data = {}
-        doc_ids = []
+    with open(in_file, newline='', encoding='utf-8-sig') as csvfile:
+        # Create a dictionary to store the mapping of docIDs as incrementing integers
+        doc_ids_dict = {}
+        dictionary = {}
+        doc_lengths = {}
 
         # Start progress bar
         indexing_progress_bar = Bar("Loading in documents", max=17153)
 
         # Read in CSV dataset
         csv_reader = csv.reader(csvfile)
+        next(csv_reader, None)  # Remove headers
 
         # Iterate over each row
-        i = 0
         for row in csv_reader:
-            if i < 1:
-                print(row)
-            i += 1
+            data_row = {}
+
+            data_row["doc_id"] = row[0]
+            data_row["title"] = row[1]
+            data_row["content"] = row[2]
+            data_row["date_posted"] = row[3]
+            data_row["court"] = row[4]
+
+            for key in data_row.keys():
+                data_row[key] = cleaner.clean(data_row[key], key)
+            print(data_row)
+
+            # sentences = nltk.sent_tokenize(text)  # Tokenize by sentence
+
+            # terms = []  # Keep track of unique terms in document
+
+            # for sentence in sentences:
+            #     words = nltk.word_tokenize(sentence)  # Tokenize by word
+
+            #     # clean out isolated punctuations
+            #     words = [w for w in words if w not in string.punctuation]
+            #     words_stemmed = [ps.stem(w) for w in words]  # Stem every word
+
+            #     for word in words_stemmed:
+            #         # Track unique terms
+            #         terms.append(word)
+
+            #         # If new term, add term to dictionary and initialize new postings list for that term
+            #         if word not in dictionary:
+
             # Update progress bar
             indexing_progress_bar.next()
 
@@ -71,138 +90,138 @@ def build_index(in_file, out_dict, out_postings):
 
     print("Documents loaded. Writing out total collection size to disk...")
 
-    # Write out collection size (number of documents) to disk
-    write_collection_size_to_disk(len(doc_ids), out_postings)
+    # # Write out collection size (number of documents) to disk
+    # write_collection_size_to_disk(len(doc_ids), out_postings)
 
-    print("Total collection size is {}.".format(len(doc_ids)))
+    # print("Total collection size is {}.".format(len(doc_ids)))
 
-    # Initialize porter stemmer
-    ps = nltk.stem.PorterStemmer()
+    # # Initialize porter stemmer
+    # ps = nltk.stem.PorterStemmer()
 
-    print("Stemming terms and tracking document lengths...")
+    # print("Stemming terms and tracking document lengths...")
 
-    # Track progress while indexing
-    processing_bar = Bar("Processing documents", max=len(doc_ids))
+    # # Track progress while indexing
+    # processing_bar = Bar("Processing documents", max=len(doc_ids))
 
-    # Create a dictionary of terms and another dictionary for document lengths
-    dictionary = {}
-    doc_lengths = {}
+    # # Create a dictionary of terms and another dictionary for document lengths
+    # dictionary = {}
+    # doc_lengths = {}
 
-    # Process every document and create a dictionary of posting lists
-    for doc_id in doc_ids:
-        # Open the document file
-        f = open(os.path.join(in_file, str(doc_id)), "r")
+    # # Process every document and create a dictionary of posting lists
+    # for doc_id in doc_ids:
+    #     # Open the document file
+    #     f = open(os.path.join(in_file, str(doc_id)), "r")
 
-        text = f.read()  # Read the document in fulltext
-        text = text.lower()  # Convert text to lower case
-        sentences = nltk.sent_tokenize(text)  # Tokenize by sentence
+    #     text = f.read()  # Read the document in fulltext
+    #     text = text.lower()  # Convert text to lower case
+    #     sentences = nltk.sent_tokenize(text)  # Tokenize by sentence
 
-        terms = []  # Keep track of unique terms in document
+    #     terms = []  # Keep track of unique terms in document
 
-        for sentence in sentences:
-            words = nltk.word_tokenize(sentence)  # Tokenize by word
+    #     for sentence in sentences:
+    #         words = nltk.word_tokenize(sentence)  # Tokenize by word
 
-            words = [
-                w for w in words if w not in string.punctuation
-            ]  # clean out isolated punctuations
-            words_stemmed = [ps.stem(w) for w in words]  # Stem every word
+    #         words = [
+    #             w for w in words if w not in string.punctuation
+    #         ]  # clean out isolated punctuations
+    #         words_stemmed = [ps.stem(w) for w in words]  # Stem every word
 
-            for word in words_stemmed:
-                # Track unique terms
-                terms.append(word)
+    #         for word in words_stemmed:
+    #             # Track unique terms
+    #             terms.append(word)
 
-                # If new term, add term to dictionary and initialize new postings list for that term
-                if word not in dictionary:
-                    dictionary[word] = {}  # Initialize new postings list
+    #             # If new term, add term to dictionary and initialize new postings list for that term
+    #             if word not in dictionary:
+    #                 dictionary[word] = {}  # Initialize new postings list
 
-                    # Update document freq for this new word to 1
-                    dictionary[word]["doc_freq"] = 1
+    #                 # Update document freq for this new word to 1
+    #                 dictionary[word]["doc_freq"] = 1
 
-                    # Create an empty posting list
-                    dictionary[word]["postings_list"] = []
+    #                 # Create an empty posting list
+    #                 dictionary[word]["postings_list"] = []
 
-                    # Add term freq to posting
-                    dictionary[word]["postings_list"].append([doc_id, 1])
-                # If term in dictionary, check if document for that term is already inside
-                else:
-                    # If doc_id already exists in postings list, simply increment term frequency in doc
-                    if dictionary[word]["postings_list"][-1][0] == doc_id:
-                        dictionary[word]["postings_list"][-1][1] += 1
-                    # Create new document in postings list and set term frequency to 1
-                    else:
-                        # Add term freq to posting
-                        dictionary[word]["postings_list"].append([doc_id, 1])
+    #                 # Add term freq to posting
+    #                 dictionary[word]["postings_list"].append([doc_id, 1])
+    #             # If term in dictionary, check if document for that term is already inside
+    #             else:
+    #                 # If doc_id already exists in postings list, simply increment term frequency in doc
+    #                 if dictionary[word]["postings_list"][-1][0] == doc_id:
+    #                     dictionary[word]["postings_list"][-1][1] += 1
+    #                 # Create new document in postings list and set term frequency to 1
+    #                 else:
+    #                     # Add term freq to posting
+    #                     dictionary[word]["postings_list"].append([doc_id, 1])
 
-                        # Update document frequency
-                        dictionary[word]["doc_freq"] += 1
+    #                     # Update document frequency
+    #                     dictionary[word]["doc_freq"] += 1
 
-        # Make set only unique terms
-        terms = list(set(terms))
+    #     # Make set only unique terms
+    #     terms = list(set(terms))
 
-        # Calculate document length (sqrt of all weights squared)
-        doc_length = 0
-        for term in terms:
-            # If term appears in doc, calculate its weight in the document W(t,d)
-            if dictionary[term]["postings_list"][-1][0] == doc_id:
-                term_weight_in_doc = 0
-                # If term frequency is more than 0 then we add to the weight
-                if dictionary[term]["postings_list"][-1][1] > 0:
-                    # Take the log frequecy weight of term t in doc
-                    # Note that we ignore inverse document frequency for documents
-                    term_weight_in_doc = 1 + math.log(
-                        dictionary[term]["postings_list"][-1][1], 10
-                    )
+    #     # Calculate document length (sqrt of all weights squared)
+    #     doc_length = 0
+    #     for term in terms:
+    #         # If term appears in doc, calculate its weight in the document W(t,d)
+    #         if dictionary[term]["postings_list"][-1][0] == doc_id:
+    #             term_weight_in_doc = 0
+    #             # If term frequency is more than 0 then we add to the weight
+    #             if dictionary[term]["postings_list"][-1][1] > 0:
+    #                 # Take the log frequecy weight of term t in doc
+    #                 # Note that we ignore inverse document frequency for documents
+    #                 term_weight_in_doc = 1 + math.log(
+    #                     dictionary[term]["postings_list"][-1][1], 10
+    #                 )
 
-                # Add term weight in document squared to total document length
-                doc_length += term_weight_in_doc ** 2
+    #             # Add term weight in document squared to total document length
+    #             doc_length += term_weight_in_doc ** 2
 
-        # Take sqrt of doc_length for final doc length
-        doc_length = math.sqrt(doc_length)
+    #     # Take sqrt of doc_length for final doc length
+    #     doc_length = math.sqrt(doc_length)
 
-        # Add final doc_length to doc_lengths dictionary
-        doc_lengths[doc_id] = doc_length
+    #     # Add final doc_length to doc_lengths dictionary
+    #     doc_lengths[doc_id] = doc_length
 
-        # Close file and update progress bar
-        f.close()
-        processing_bar.next()
+    #     # Close file and update progress bar
+    #     f.close()
+    #     processing_bar.next()
 
-    # Update progress bar
-    processing_bar.finish()
-    print("Pre-processing complete. Writing document lengths to disk...")
+    # # Update progress bar
+    # processing_bar.finish()
+    # print("Pre-processing complete. Writing document lengths to disk...")
 
-    # Save doc_lengths to disk
-    write_doc_lengths_to_disk(doc_lengths)
+    # # Save doc_lengths to disk
+    # write_doc_lengths_to_disk(doc_lengths)
 
-    print("{} document lengths written to disk.".format(len(doc_ids)))
+    # print("{} document lengths written to disk.".format(len(doc_ids)))
 
-    # Create dictionary of K:V {term: Address to postings list of that term}
-    term_dict = {}
+    # # Create dictionary of K:V {term: Address to postings list of that term}
+    # term_dict = {}
 
-    # Track progress while indexing
-    print("Indexing terms and saving each postings list to disk...")
-    indexing_bar = Bar("Indexing terms", max=len(dictionary.keys()))
+    # # Track progress while indexing
+    # print("Indexing terms and saving each postings list to disk...")
+    # indexing_bar = Bar("Indexing terms", max=len(dictionary.keys()))
 
-    # For each term, split into term_dict and PostingsList, and write out to their respective files
-    for term in dictionary.keys():
-        # Write PostingsList for each term out to disk and get its address
-        ptr = write_postings_list_to_disk(dictionary[term], out_postings)
+    # # For each term, split into term_dict and PostingsList, and write out to their respective files
+    # for term in dictionary.keys():
+    #     # Write PostingsList for each term out to disk and get its address
+    #     ptr = write_postings_list_to_disk(dictionary[term], out_postings)
 
-        # Update term_dict with the address of the PostingsList for that term
-        term_dict[term] = ptr
+    #     # Update term_dict with the address of the PostingsList for that term
+    #     term_dict[term] = ptr
 
-        # Update progress bar
-        indexing_bar.next()
+    #     # Update progress bar
+    #     indexing_bar.next()
 
-    # Update progress bar
-    indexing_bar.finish()
-    print("Posting lists saved to disk.")
+    # # Update progress bar
+    # indexing_bar.finish()
+    # print("Posting lists saved to disk.")
 
-    # Track progress while indexing
-    print("Saving term dictionary to disk...")
+    # # Track progress while indexing
+    # print("Saving term dictionary to disk...")
 
-    # Now the term_dict has the pointers to each terms' PostingsList
-    # Write out the dictionary to the dictionary file on disk
-    write_dictionary_to_disk(term_dict, out_dict)
+    # # Now the term_dict has the pointers to each terms' PostingsList
+    # # Write out the dictionary to the dictionary file on disk
+    # write_dictionary_to_disk(term_dict, out_dict)
 
     print("Indexing complete.")
 
