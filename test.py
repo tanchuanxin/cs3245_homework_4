3d097a6ee593d2cc543ced78dcb93dcdd422684c
Chen Ning <chenningg97@gmail.com>
2021-04-19 15:04:19 +0800

    Added test.py

---
+++ test.py
@@ -0,0 +1,47 @@
+import os
+import pickle
+
+# Loads in the term dictionary from file
+
+
+def load_postings():
+    f_postings = open(
+        os.path.join(os.path.dirname(__file__), "postings.txt"), "rb"
+    )
+
+    postings = pickle.load(f_postings)
+    f_postings.close()
+    print(postings)
+
+
+def load_dictionary():
+    f_dict = open(
+        os.path.join(os.path.dirname(__file__), "dictionary.txt"), "rb"
+    )
+
+    dictionary = pickle.load(f_dict)
+    f_dict.close()
+    print(dictionary)
+
+
+def load_metadata():
+    f_metadata = open(
+        os.path.join(os.path.dirname(__file__), "metadata.txt"), "rb"
+    )
+
+    metadata = pickle.load(f_metadata)
+    f_metadata.close()
+    print(metadata)
+
+
+def load_doc_lengths():
+    f_doc_lengths = open(
+        os.path.join(os.path.dirname(__file__), "doc_lengths.txt"), "rb"
+    )
+
+    doc_lengths = pickle.load(f_doc_lengths)
+    f_doc_lengths.close()
+    print(doc_lengths)
+
+
+load_doc_lengths()
